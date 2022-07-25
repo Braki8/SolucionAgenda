9388a8b7fecdbee1555a3d2e56754c26c4e1dbd5
Bryan_Itla <105371494+Braki8@users.noreply.github.com>
2022-07-24 20:37:11 -0400

    Agregar archivos de proyecto.

---
+++ Capa_Negocio/N_Empleados.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using capa_Entidad;
+using System.Data;
+
+
+namespace Capa_Negocio
+{
+    public class N_Empleados
+    {
+
+        D_Empleados objdato = new D_Empleados();
+
+        public DataTable N_listado()
+        {
+            return objdato.D_listado();
+        }
+
+    }
+}
---
+++ capa_Entidad/E_Empleados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa_Entidad
+{
+    public class E_Empleados
+    {
+
+        public int cod { get; set; }
+
+        public int nom { get; set; }
+
+        public int edad { get; set; }
+
+        public int sexo { get; set; }
+
+        public int sueldo { get; set; }
+
+    }
+}
